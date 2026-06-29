e36ab2f47d8430c5c76c39cb3d24d7900f542e53
Jyoti Sahu <jyoti.sahu@fmr.com>
2026-06-29 14:06:34 -0600

    fix:remove spring-6 from sonar coverage & add sql scripts for 3.0

--- engine/src/main/resources/org/finos/fluxnova/bpm/engine/db/upgrade/h2_engine_2.0_to_3.0.sql
+++ engine/src/main/resources/org/finos/fluxnova/bpm/engine/db/upgrade/h2_engine_2.0_to_3.0.sql
@@ -6,11 +6,12 @@
 -- SPDX-License-Identifier: Apache-2.0
 --
 
-alter table ACT_RU_VARIABLE add column RESTRICTED_ bit;
-alter table ACT_HI_VARINST add column RESTRICTED_ bit;
+alter table ACT_RU_VARIABLE add column if not exists RESTRICTED_ bit;
+alter table ACT_HI_VARINST add column if not exists RESTRICTED_ bit;
+alter table ACT_HI_ATTACHMENT add column if not exists CREATED_BY_ varchar(255);
 
-create index ACT_IDX_VARIABLE_RESTRICTED on ACT_RU_VARIABLE(RESTRICTED_);
-create index ACT_IDX_HI_VARINST_RESTRICTED on ACT_HI_VARINST(RESTRICTED_);
+create index if not exists ACT_IDX_VARIABLE_RESTRICTED on ACT_RU_VARIABLE(RESTRICTED_);
+create index if not exists ACT_IDX_HI_VARINST_RESTRICTED on ACT_HI_VARINST(RESTRICTED_);
 
 insert into ACT_GE_SCHEMA_LOG
 values ('1500', CURRENT_TIMESTAMP, '3.0.0');
